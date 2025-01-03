1256f428857787ae43a7cb54997d6f9d0c562c4c
Anish Singla <singlaanish56@gmail.com>
2025-01-03 23:26:48 +0530

    Viewport gradient

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,58 @@
 #include<iostream>
 #include"color.hpp"
 #include"vec3.hpp"
+#include"ray.hpp"
 
+color ray_color(const ray& r)
+{
+    vec3 unit_direction  = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a) * color(1.0,1.0,1.0) + a * color(0.5,0.7,1.0);
+}
 
 int main(){
 
-    int image_wdith = 256;
-    int image_height = 256;
-    std::cout<<"P3\n"<< image_wdith <<' '<<image_height<<"\n255\n";
+
+    //Image
+    auto aspect_ratio = 16.0/9.0;
+    int image_width = 400;
+
+    //calculate the height, depending on the width
+    int image_height =int(image_width / aspect_ratio);
+    image_height = image_height <1 ? 1 : image_height;
+
+
+    //camera
+    auto focal_length = 1.0;
+    auto camera_center = point3(0,0,0);
+
+    //viewport
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width/image_height));
+    auto viewport_u = vec3(viewport_width ,0 ,0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+    auto pixel_u_delta =  viewport_u / image_width;
+    auto pixel_v_delta = viewport_v / image_height; 
+
+    //the upper left location of the pixel on the viewport
+    auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+
+    auto pixel00_loc = viewport_upper_left + 0.5 *( pixel_u_delta+pixel_v_delta);
+
+    
+
+    std::cout<<"P3\n"<< image_width <<' '<<image_height<<"\n255\n";
     
     for(int j=0;j<image_height;j++){
         std::clog<<"\rScanline remaining: " <<(image_height-j) << ' '<<std::flush;
-        for(int i=0;i<image_wdith;i++){
-            auto r = double(i) / (image_wdith-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
+        for(int i=0;i<image_width;i++){
+            auto pixel_center = pixel00_loc + (i* pixel_u_delta) + (j * pixel_v_delta);
+            auto ray_direction =  pixel_center - camera_center;
 
-            auto pixels = color(r,g,b);
-            write_color(std::cout, pixels); 
+            ray r ( camera_center, ray_direction);
+            color pixel_color = ray_color(r);
+
+            write_color(std::cout, pixel_color); 
         }
     }
 
