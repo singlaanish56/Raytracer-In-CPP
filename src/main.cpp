bbc9ad2ccf1552b237d24625440b4e4669c458be
Anish Singla <singlaanish56@gmail.com>
2025-01-05 00:32:51 +0530

    generating simple gradient sphere

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,32 @@
 #include"vec3.hpp"
 #include"ray.hpp"
 
+double hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+
+    auto b = -2.0 * dot(r.direction(), oc);
+
+    auto c =dot(oc,oc) - radius* radius;
+
+    auto discriminant = b*b - 4*a*c;
+
+    if (discriminant < 0){
+        return -1.0;
+
+    }
+
+    return (-b-std::sqrt(discriminant))/(2.0*a);
+}
+
 color ray_color(const ray& r)
-{
+{   
+    double t = hit_sphere(point3(0,0,-1),0.5,r);
+    if (t >0.0){
+        vec3 n = unit_vector(r.at(t)- vec3(0,0,-1));
+        return 0.5 * color(n.x()+1,n.y()+1,n.z()+1);
+    }
+
     vec3 unit_direction  = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a) * color(1.0,1.0,1.0) + a * color(0.5,0.7,1.0);
